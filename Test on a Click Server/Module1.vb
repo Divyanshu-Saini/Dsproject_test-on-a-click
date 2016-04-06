8eface5f7a5483d9fa5342a6fa97edd20b765921
Divyanshu Saini <bat2268@gmail.com>
2016-04-06 12:38:35 +0530

    updated files

---
+++ Test on a Click Server/Module1.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+Imports System.Data
+Imports System.Runtime
+Imports System.Linq
+Imports System.Text
+Imports System.Configuration
+Imports System.Threading.Tasks
+Imports System.Collections.Generic
+Imports System.Data.SqlClient
+Imports System.Runtime.Remoting
+Imports System.Runtime.Remoting.Channels
+Imports System.Runtime.Remoting.Channels.Tcp
+Module Module1
+
+    Sub Main()
+        Dim tcpChannel As IChannel
+        tcpChannel = New TcpServerChannel("8085")
+        ChannelServices.RegisterChannel(tcpChannel, False)
+        RemotingConfiguration.RegisterWellKnownServiceType(GetType(RemotableObject.RemoteClass), "MyRemoteObject", WellKnownObjectMode.SingleCall)
+        Console.WriteLine("Starting server ...")
+        Console.WriteLine("Server started.")
+        Console.ReadLine()
+    End Sub
+
+End Module
